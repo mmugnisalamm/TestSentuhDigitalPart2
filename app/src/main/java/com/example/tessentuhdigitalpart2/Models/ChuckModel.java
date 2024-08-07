18e4305ee8d147136ae6852922a38f8afe11e1f1
mmugnisalamm <muhammad.mugnisalam@gmail.com>
2024-08-07 21:10:37 +0700

    feat: First Commit, pembuatan MainActivity.class, ChuckAdapter.java, ChuckModel.java, activity_main.xml, item_chuck.xml

---
+++ app/src/main/java/com/example/tessentuhdigitalpart2/Models/ChuckModel.java
@@ -0,0 +1,64 @@
+package com.example.tessentuhdigitalpart2.Models;
+
+import java.util.ArrayList;
+
+public class ChuckModel {
+    ArrayList<String> categories;
+    String created_at, icon_url, id, updated_at, url, value;
+
+    public ArrayList<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(ArrayList<String> categories) {
+        this.categories = categories;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getIcon_url() {
+        return icon_url;
+    }
+
+    public void setIcon_url(String icon_url) {
+        this.icon_url = icon_url;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
